1dface0074bd00b86050df53c48bd2547b89991b
Wai Yan Aung <wya.aung@gmail.com>
2023-06-02 19:24:11 +0100

    Partially Implemented Player Class
    - Player Constructor
    - getScore Method
    - getHandLimit Method
    - getStickNumber Method
    - addSticks Method
    - removeSticks Method
    - getHand Method
    - getDisplay Method
    - addCardtoHand Method
    - addCardtoDisplay Method

--- src/board/Player.java
+++ src/board/Player.java
@@ -3,6 +3,9 @@ package board;
 import java.util.ArrayList;
 
 import cards.Card;
+import cards.CardType;
+import cards.Pan;
+import cards.Stick;
 
 public class Player {
     private Hand hand;
@@ -12,7 +15,17 @@ public class Player {
     private int handLimit;
     private int sticks;
 
-    public Player() {}
+    public Player() {
+        this.score = 0;
+        this.handLimit = 8;
+        this.sticks = 0;
+
+        this.hand = new Hand();
+        this.display = new Display();
+
+        // Adding Default Pan
+        this.display.add(new Pan());
+    }
 
     public int getScore() {
         return this.score;
@@ -26,9 +39,25 @@ public class Player {
         return this.sticks;
     }
 
-    public void addSticks(int numOfSticks) {}
+    public void addSticks(int numOfSticks) {
+        while (numOfSticks > 0){
+            this.display.add(new Stick());
+            numOfSticks--;
+        }
+    }
 
-    public void removeSticks(int numOfSticks) {}
+    public void removeSticks(int numOfSticks) {
+        if (this.sticks < numOfSticks){
+            return;
+        }
+
+        for (int i = 0; i < this.display.size(); i++) {
+            if ((this.display.getElementAt(i).getType() == CardType.STICK) && (numOfSticks > 0)) {
+                this.display.removeElement(i);
+                numOfSticks--;
+            }
+        }
+    }
 
     public Hand getHand() {
         return this.hand;
@@ -38,9 +67,18 @@ public class Player {
         return this.display;
     }
 
-    public void addCardtoHand(Card card) {}
+    public void addCardtoHand(Card card) {
+        if (card.getType() == CardType.BASKET) {
+            this.display.add(card);
+            this.handLimit += 2;
+        } else {
+            this.hand.add(card);
+        }
+    }
 
-    public void addCardtoDisplay(Card card) {}
+    public void addCardtoDisplay(Card card) {
+        this.display.add(card);
+    }
 
     public boolean takeCardFromTheForest(int index) {
         return false;
